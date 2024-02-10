5e40aa58e20fda8e1b54c985df2e0fb81858f9e8
Aidan Ceney <aidanceney@gmail.com>
2024-02-09 22:12:29 -0700

    Added Dino Movement

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -1,19 +1,25 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CharacterController : MonoBehaviour
 {
     public Vector3 velocity;
+    public float walkAcceleration = 5.0f;
+    public float groundDeceleration = 10.0f;
+    public float maxSpeed = 40f;
     public float speed = 1f;
-    public float walkAcceleration = 0.2f;
-    public float groundDeceleration = 0.5f;
-    public float maxSpeed = 10f;
+    public float jumpAcceleration = 25.0f;
+    private bool grounded = false;
+    public Rigidbody2D rb;
+    
+
 
     void Start()
     {
-        
+    rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -22,20 +28,25 @@ public class CharacterController : MonoBehaviour
         float moveInput = Input.GetAxisRaw("Horizontal");
         if (moveInput != 0)
         {
-            velocity.x = Math.Min(Mathf.MoveTowards(velocity.x, speed * moveInput, walkAcceleration * Time.deltaTime), maxSpeed);
+            rb.AddForce(new Vector2(moveInput * walkAcceleration * Time.deltaTime,0.0f),ForceMode2D.Impulse);
+            Debug.Log("Walk: " +  velocity.x);
         }
         else
         {
-            velocity.x = Mathf.MoveTowards(velocity.x, 0, groundDeceleration * Time.deltaTime);
         }
-        transform.Translate(velocity * Time.deltaTime);
+        moveInput = Input.GetAxisRaw("Vertical");
+        if (moveInput != 0 && grounded)
+        {
+            rb.AddForce(new Vector2(0.0f,1.0f) * jumpAcceleration, ForceMode2D.Impulse);
+            grounded = false;
+        }
     }
 
     void OnCollisionEnter2D(Collision2D col)
     {
         Debug.Log(col.gameObject.tag);
-        if(col.gameObject.CompareTag("Coin")){
-            Destroy(col.gameObject);
+        if(col.gameObject.CompareTag("Ground")){
+            grounded = true;
         }
     }
 }
