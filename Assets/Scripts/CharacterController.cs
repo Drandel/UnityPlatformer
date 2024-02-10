e5471fb263b25f14c58430ec14020cefe32c3f03
Aidan Ceney <aidanceney@gmail.com>
2024-02-09 22:24:00 -0700

    Revert "Added Dino Movement"
    This reverts commit 5e40aa58e20fda8e1b54c985df2e0fb81858f9e8.

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -1,25 +1,19 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class CharacterController : MonoBehaviour
 {
     public Vector3 velocity;
-    public float walkAcceleration = 5.0f;
-    public float groundDeceleration = 10.0f;
-    public float maxSpeed = 40f;
     public float speed = 1f;
-    public float jumpAcceleration = 25.0f;
-    private bool grounded = false;
-    public Rigidbody2D rb;
-    
-
+    public float walkAcceleration = 0.2f;
+    public float groundDeceleration = 0.5f;
+    public float maxSpeed = 10f;
 
     void Start()
     {
-    rb = GetComponent<Rigidbody2D>();
+        
     }
 
     // Update is called once per frame
@@ -28,25 +22,20 @@ public class CharacterController : MonoBehaviour
         float moveInput = Input.GetAxisRaw("Horizontal");
         if (moveInput != 0)
         {
-            rb.AddForce(new Vector2(moveInput * walkAcceleration * Time.deltaTime,0.0f),ForceMode2D.Impulse);
-            Debug.Log("Walk: " +  velocity.x);
+            velocity.x = Math.Min(Mathf.MoveTowards(velocity.x, speed * moveInput, walkAcceleration * Time.deltaTime), maxSpeed);
         }
         else
         {
+            velocity.x = Mathf.MoveTowards(velocity.x, 0, groundDeceleration * Time.deltaTime);
         }
-        moveInput = Input.GetAxisRaw("Vertical");
-        if (moveInput != 0 && grounded)
-        {
-            rb.AddForce(new Vector2(0.0f,1.0f) * jumpAcceleration, ForceMode2D.Impulse);
-            grounded = false;
-        }
+        transform.Translate(velocity * Time.deltaTime);
     }
 
     void OnCollisionEnter2D(Collision2D col)
     {
         Debug.Log(col.gameObject.tag);
-        if(col.gameObject.CompareTag("Ground")){
-            grounded = true;
+        if(col.gameObject.CompareTag("Coin")){
+            Destroy(col.gameObject);
         }
     }
 }
